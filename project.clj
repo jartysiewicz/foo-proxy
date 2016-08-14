0450600820da584c81443b0a506fb75356961d36
jstaffans <johannes.staffans@gmail.com>
2016-08-14 23:03:40 +0200

    Logging configuration

--- project.clj
+++ project.clj
@@ -5,10 +5,12 @@
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.logging "0.3.1"]
+                 [org.slf4j/slf4j-log4j12 "1.7.1"]
                  [org.clojure/core.async "0.2.385"] ; CSP for Clojure
                  [beckon "0.1.1"]                   ; POSIX signal handling
                                                     ; (a black art on the JVM)
                  ]
+  :resources ["resources"]
   :main foo-proxy.core
   :profiles {:uberjar {:aot :all}
              :dev     {:source-paths ["dev"]
