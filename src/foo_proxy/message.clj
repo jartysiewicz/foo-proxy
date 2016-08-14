e0c27dc4f66a9f05e442ad802db8149b8613d35b
jstaffans <johannes.staffans@gmail.com>
2016-08-14 16:56:47 +0200

    Optimize message parsing

--- src/foo_proxy/message.clj
+++ src/foo_proxy/message.clj
@@ -2,12 +2,9 @@
   (:require [clojure.string :as string]))
 
 (defn parse-message-type
-  "Parses a message like \"REQ 1 Foobar\" into a message type, e.g. \":req\".
-  A more efficient way would be to work on the byte level all the way, the
-  string handling is a bit unnecessary."
+  "Parses a message like \"REQ 1 Foobar\" into a message type, e.g. \":req\"."
   [s]
   (-> s
-      (string/split #" ")
-      first
+      (subs 0 3)
       .toLowerCase
       keyword))
